aba3f61f41d25df74f6cb1d1d427a7e6ef9ad7a9
elivanov.denis <elivanov.denis@mediasoft.team>
2019-03-19 11:29:58 +0400

    add delete

--- routes/data.js
+++ routes/data.js
@@ -7,9 +7,9 @@ router.get('/groups', controller.getGroups);
 router.get('/timeLesson', controller.getTimeLesson);
 router.get('/subjects', controller.getSubjects);
 router.get('/group/:id', controller.getGroupId);
+router.get('/group-delete/:id', controller.deleteGroup);
 router.post('/group-create', controller.createGroup);
-router.post('/group-change', controller.changeGroup);
-router.post('/group-delete/:id', controller.deleteGroup);
+router.post('/group-change',  controller.changeGroup);
 
 
 module.exports = router;
